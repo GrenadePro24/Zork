9ba3d3678c5e39532f21e89ca1a933616f3c8ffc
Gabriel Rivas <GrenadePro24@users.noreply.github.com>
2019-05-20 22:47:05 +0200

    Added missing classes
    Added missing classes due to accidental *.h in .gitignore

---
+++ Init.h
@@ -0,0 +1,29 @@
+#ifndef Init_
+#define Init_
+#include <list>
+#include <string>
+#include "Item.h"
+#include "Warp.h"
+#include "Room.h"
+#include "NPC.h"
+
+class Init
+{
+public:
+	list<Item> inventory;
+	list<Item> items;
+	list<Room> rooms;
+	list<Warp> warps;
+	list<NPC> NPCs;
+	Room currentRoom = Room("","");
+	string instructions;
+
+	void initiateRooms();
+	void initiateItems();
+	void initiateWarps();
+	void initiateNPCs();
+	void loadRoom(Room currentRoom);
+};
+
+#endif
+
